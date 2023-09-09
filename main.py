1f6714713a41ca802e26685ecf2274ca4d582238
Jaiakash73 <144465583+Jaiakash73@users.noreply.github.com>
2023-09-09 08:38:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Leaf year
+"""
+year % 4 == 0&
+year % 100 != 0 /
+year % 400 == 0
+
+"""
+
+
+def isLeapYear(year):
+  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True
+  else:
+
+    return False
+
+
+year = 2004
+
+if isLeapYear(year):
+  print('{} is a leap year.'.format(isLeapYear))
+else:
+  print('{} is not a Leap Year.'.format(year))
